503c90404ae12afa1a0f2e29ef47d1d5a4bc3a3f
Karim Yasser <104207447+karimyasser55555@users.noreply.github.com>
2023-04-20 07:52:14 +0200

    Add files via upload

---
+++ WinFormsApp5/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(string checkedItems in checkedListBox1.CheckedItems.OfType<string>().ToList())
+            {
+                checkedListBox2.Items.Add(checkedItems);
+                checkedListBox1.Items.Remove(checkedItems);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+            foreach (string checkedItems in checkedListBox1.CheckedItems.OfType<string>().ToList())
+            {
+                checkedListBox2.Items.Add(checkedItems);
+                checkedListBox1.Items.Remove(checkedItems);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (string checkedItems in checkedListBox2.CheckedItems.OfType<string>().ToList())
+            {
+                checkedListBox1.Items.Add(checkedItems);
+                checkedListBox2.Items.Remove(checkedItems);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox2.Items.Count; i++)
+            {
+                checkedListBox2.SetItemChecked(i, true);
+            }
+            foreach (string checkedItems in checkedListBox2.CheckedItems.OfType<string>().ToList())
+            {
+                checkedListBox1.Items.Add(checkedItems);
+                checkedListBox2.Items.Remove(checkedItems);
+            }
+        }
+    }
+}
