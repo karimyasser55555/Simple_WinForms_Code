c3ca981a9c9a922e5d64b8195defe6f839a0eb1f
Karim Yasser <104207447+karimyasser55555@users.noreply.github.com>
2023-04-20 07:52:58 +0200

    Add files via upload

---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Name";
+            dataGridView1.Columns[1].Name = "Phone";
+            dataGridView1.Columns[2].Name = "Birth Date";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, dateTimePicker1.Value);
+        }
+    }
+}
